66492fbc163bef4c6bb2ac640c5252c3acc5392a
pinakinmistry <pinakin.nmistry@gmail.com>
2015-01-03 13:58:06 +0530

    Title for slides with code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -28,6 +28,7 @@ angular.module('trainingApp')
       },
       {
         type: 'code',
+        title: 'Edit/run code',
         content: "(function() { \n\
           return 'hi'; \n\
         })()",
